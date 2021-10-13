2ec0bdf6299730bb3125df53a89e288ada724433
roluochKe <rasholuoch@gmail.com>
2021-10-13 22:33:44 +0300

    game complete

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -1,20 +1,41 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let player = {
+  name: 'Ray',
+  chips: 145,
+}
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ''
 
 let messageEl = document.getElementById('message-el')
 let sumEl = document.querySelector('#sum-el')
 let cardsEl = document.getElementById('cards-el')
+let playerEl = document.getElementById('player-el')
+
+playerEl.textContent = player.name + ': $' + player.chips
 
 function getRandomCard() {
-  return Math.floor(Math.random() * 13) + 1
+  let randomNumber = Math.floor(Math.random() * 13) + 1
+
+  if (randomNumber > 10) {
+    return 10
+  } else if (randomNumber === 1) {
+    return 11
+  } else {
+    return randomNumber
+  }
 }
 
 function startGame() {
+  isAlive = true
+
+  let firstCard = getRandomCard()
+  let secondCard = getRandomCard()
+
+  cards = [firstCard, secondCard]
+  sum = firstCard + secondCard
+
   renderGame()
 }
 
@@ -40,10 +61,12 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = getRandomCard()
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard()
 
-  sum += card
-  cards.push(card)
+    sum += card
+    cards.push(card)
 
-  renderGame()
+    renderGame()
+  }
 }
