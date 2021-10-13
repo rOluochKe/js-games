7703d1d9e7d09c05b49b984841aa6a5d1d510a3f
roluochKe <rasholuoch@gmail.com>
2021-10-13 21:45:19 +0300

    getrandomcard, startgame, rendergame and newcard logic

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -1,19 +1,49 @@
-let firstCard = 6
-let secondCard = 20
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ''
 
-if (sum <= 20) {
-  message = 'Do you want to draw a new card?'
-} else if (sum === 21) {
-  message = "Wohoo! You've got a blackjack!"
-  hasBlackJack = true
-} else {
-  message = "You're out of the game!"
-  isAlive = false
+let messageEl = document.getElementById('message-el')
+let sumEl = document.querySelector('#sum-el')
+let cardsEl = document.getElementById('cards-el')
+
+function getRandomCard() {
+  return Math.floor(Math.random() * 13) + 1
 }
 
-// cash out
-console.log(message)
+function startGame() {
+  renderGame()
+}
+
+function renderGame() {
+  cardsEl.textContent = 'Cards: '
+
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + ' '
+  }
+
+  sumEl.textContent = 'Sum: ' + sum
+
+  if (sum <= 20) {
+    message = 'Do you want to draw a new card?'
+  } else if (sum === 21) {
+    message = "You've got a blackjack!"
+    hasBlackJack = true
+  } else {
+    message = "You're out of the game!"
+    isAlive = false
+  }
+  messageEl.textContent = message
+}
+
+function newCard() {
+  let card = getRandomCard()
+
+  sum += card
+  cards.push(card)
+
+  renderGame()
+}
