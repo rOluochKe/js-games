bc9a348cbeaf24f87bb9a10f5847da8cbca0a530
roluochKe <rasholuoch@gmail.com>
2021-10-14 17:47:54 +0300

    displaying list of leads

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,7 +1,20 @@
-let myLeads = []
+let myLeads = ['www.lead-one.com', 'www.lead-two.com', 'www.lead-three.com']
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
+const ulEl = document.getElementById('ul-el')
 
 inputBtn.addEventListener('click', function () {
-  console.log('clicked me')
+  myLeads.push(inputEl.value)
+  console.log(myLeads)
 })
+
+let listItems = ''
+for (let i = 0; i < myLeads.length; i++) {
+  listItems += '<li>' + myLeads[i] + '</li>'
+
+  // const li = document.createElement('li')
+  // li.textContent = myLeads[i]
+  // ulEl.append(li)
+}
+
+ulEl.innerHTML = listItems
