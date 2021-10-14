0db230ef7c2e78ed26efc7b92fa96cc5cb63d091
roluochKe <rasholuoch@gmail.com>
2021-10-14 18:03:42 +0300

    open href in a new tab

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,20 +1,25 @@
-let myLeads = ['www.lead-one.com', 'www.lead-two.com', 'www.lead-three.com']
+let myLeads = []
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
 
 inputBtn.addEventListener('click', function () {
   myLeads.push(inputEl.value)
-  console.log(myLeads)
+  inputEl.value = ''
+  renderLeads()
 })
 
-let listItems = ''
-for (let i = 0; i < myLeads.length; i++) {
-  listItems += '<li>' + myLeads[i] + '</li>'
+function renderLeads() {
+  let listItems = ''
 
-  // const li = document.createElement('li')
-  // li.textContent = myLeads[i]
-  // ulEl.append(li)
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems +=
+      "<li><a href='" +
+      myLeads[i] +
+      "' target='_blank'>" +
+      myLeads[i] +
+      '</a></li>'
+  }
+
+  ulEl.innerHTML = listItems
 }
-
-ulEl.innerHTML = listItems
