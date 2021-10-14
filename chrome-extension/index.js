1e06164886de41fc5de5ee6bc7c0829bf30b2d37
roluochKe <rasholuoch@gmail.com>
2021-10-14 21:00:46 +0300

    adding current tab

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -3,37 +3,46 @@ const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
 const deleteBtn = document.getElementById('delete-btn')
+const tabBtn = document.getElementById('tab-btn')
 
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
+const leadsFromsLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
 
-if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage
-  renderLeads()
+if (leadsFromsLocalStorage) {
+  myLeads = leadsFromsLocalStorage
+  render(myLeads)
 }
 
-deleteBtn.addEventListener('dblclick', function () {
-  localStorage.clear()
-  myLeads = []
-  renderLeads()
+tabBtn.addEventListener('click', function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+    render(myLeads)
+  })
 })
 
-inputBtn.addEventListener('click', function () {
-  myLeads.push(inputEl.value)
-  inputEl.value = ''
-  localStorage.setItem('myLeads', JSON.stringify(myLeads))
-  renderLeads()
-})
-
-function renderLeads() {
+function render(leads) {
   let listItems = ''
 
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `<li>
-        <a href='${myLeads[i]}' target='_blank'>
-        ${myLeads[i]}
+        <a href='${leads[i]}' target='_blank'>
+        ${leads[i]}
         </a>
       </li>`
   }
 
   ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener('dblclick', function () {
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+})
+
+inputBtn.addEventListener('click', function () {
+  myLeads.push(inputEl.value)
+  inputEl.value = ''
+  localStorage.setItem('myLeads', JSON.stringify(myLeads))
+  render(myLeads)
+})
